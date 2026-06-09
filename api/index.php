4bc394de0b617ab9ab7137e5d1cb29befd311501
Pawan Shekhawat <shekhawatpawan2402@gmail.com>
2026-06-09 15:32:54 +0530

    debug: enable PHP error reporting on Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
+// Enable error display for debugging on Vercel deployment
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Forward Vercel requests to the normal Laravel public/index.php entry point.
 require __DIR__ . '/../public/index.php';
