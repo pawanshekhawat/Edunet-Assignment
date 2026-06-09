19976850151debe3860161ffa2a2639a5fdbe218
Pawan Shekhawat <shekhawatpawan2402@gmail.com>
2026-06-09 13:38:37 +0530

    Initial commit: base Laravel project (starter code)

---
+++ resources/views/form.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('content')
+				<div class="app-content">
+					<div class="side-app">
+						<!--Page-Header-->
+						<div class="page-header">
+							<h4 class="page-title">{{$title}}</h4>
+						</div>
+                    </div>
+                </div>
+@endsection
